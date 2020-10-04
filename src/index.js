6cc671b7ca6f462b2390529e3abc920a43b8eb79
Quan Xue <33763102+quan-xue@users.noreply.github.com>
2020-10-04 08:24:18 +0800

    Complete Name of default export does not have to match when importing; names of exports have to match when importing; wildcard import discouraged

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import pie from "./math.js"; //the name of default export does not have to match
+import { doublePi, triplePi } from "./math.js"; //names of non defaults have to match
+import * as pi from "./math.js"; //import entire package as object
 
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <li>{pie}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+    <li>{pi.default}</li>
   </ul>,
   document.getElementById("root")
 );
---
+++ src/math.js
@@ -0,0 +1,12 @@
+const pi = 3.1415962;
+
+function doublePi() {
+  return 2 * pi;
+}
+
+function triplePi() {
+  return 3 * pi;
+}
+
+export default pi;
+export { doublePi, triplePi };
